22e75113128b46aac467b7ad5fc0258eae202c4e
Moreno <amore127@ford.com>
2025-08-10 23:55:06 -0600

    test

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, abort
 
 app = Flask(__name__)
 
 @app.route("/")
-def mostrar_documento():
-    return render_template("documento.html")
+def home():
+    return render_template("documento.html", id=id_param)
+
+
+@app.route("/verificar")
+def verif():
+    return render_template("documento.html", id=id_param)
+
+@app.route("/valdoc/")
+def valdoc():
+    id_param = request.args.get("id")
+    if not id_param:
+        abort(400, description="Falta el parámetro id")
+
+    if id_param != "e67b8930d6653a04f380833a5f10ae617fdb760cb2b7b945e9b7a6463d018b74":
+        return render_template("doc2.html", id=id_param)
+
+    return render_template("documento.html", id=id_param)
 
 if __name__ == "__main__":
     app.run(debug=True)
